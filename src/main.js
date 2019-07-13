30203d8b7eb5171e91822e7c1d460d9c711c1532
digi0ps <sriru1998@gmail.com>
2019-07-13 16:32:27 +0530

    Dynamically add pug files and enable hot reload.

---
+++ src/main.js
@@ -0,0 +1,3 @@
+/* Hot reload only happens if the file is in the dependency tree */
+import "./index.pug";
+import "./contact.pug";
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,42 @@
 const path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-var HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
+const fs = require("fs");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPugPlugin = require("html-webpack-pug-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+const SRC = "src";
+
+/* Dynamically add pug files */
+
+const pugs = [];
+
+const files = fs.readdirSync(path.resolve(__dirname, SRC));
+
+const decideFileStructure = filename =>
+  filename === "index" ? filename : `${filename}/index`;
+
+files.forEach(file => {
+  if (file.match(/\.pug$/)) {
+    let filename = file.substring(0, file.length - 4);
+
+    pugs.push(
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, SRC, file),
+        filename: decideFileStructure(filename) + ".html",
+      }),
+    );
+  }
+});
 
 module.exports = {
   mode: "development",
 
-  entry: ["./src/main.js"],
+  entry: path.resolve(__dirname, SRC, "main.js"),
 
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].bundle.js",
   },
-
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/index.pug",
-    }),
-    new HtmlWebpackPugPlugin(),
-  ],
 
   module: {
     rules: [
@@ -28,7 +47,11 @@ module.exports = {
     ],
   },
 
+  plugins: [new CleanWebpackPlugin(), ...pugs, new HtmlWebpackPugPlugin()],
+
   devServer: {
     port: 3000,
+    compress: true,
+    hot: true,
   },
 };
