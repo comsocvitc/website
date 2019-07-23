c395a4f741385623aef85d3a4dda4d8d2a9a11e9
digi0ps <sriru1998@gmail.com>
2019-07-23 13:20:23 +0530

    Update firebase function.

--- functions/index.js
+++ functions/index.js
@@ -1,36 +1,36 @@
 const functions = require('firebase-functions');
 const nodemailer = require('nodemailer');
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
-});
-
-
-const gmailEmail = functions.config().gmail.email;
-const gmailPassword = functions.config().gmail.password;
-const mailTransport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: gmailEmail,
-    pass: gmailPassword,
-  },
-});
-
-exports.sendEmail = functions.https.onRequest((request, response) => {
-  if (request.method !== 'POST' && request.body.type !== 'mailer') {
-    return null
+exports.sendEmail = functions.https.onRequest(async (request, response) => {
+  if (request.method !== 'POST') {
+    response.send("Only valid POST alowed")
   }
 
-  const mailOptions = {
-    from: `${APP_NAME} <noreply@firebase.com>`,
-    to: 'vitc.comsoc@gmail.com',
-  };
+  const gmailEmail = functions.config().gmail.email;
+  const gmailPassword = functions.config().gmail.password;
 
-  // The user subscribed to the newsletter.
-  mailOptions.subject = request.body.subject;
-  mailOptions.text = request.body.text;
+  const mailTransport = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: gmailEmail,
+      pass: gmailPassword,
+    },
+  });
+
+  const { subject, text } = request.body;
+
+  const mailOptions = {
+    from: `Contact Form <noreply@firebase.com>`,
+    to: 'sriru1998@gmail.com',
+    subject,
+    text,
+  };
   
-  await mailTransport.sendMail(mailOptions);
-  console.log('New email sent');
+  try {
+    await (mailTransport.sendMail(mailOptions));
+  } catch(e) {
+    response.status(500).send(`Error: ${e}`);
+  }
+  response.status(200).send("Success");
   return null;
 });
